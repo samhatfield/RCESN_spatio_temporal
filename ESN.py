48d2242617825a2993e8bb04338d40573bd3922d
Sam Hatfield <samuel.hatfield@physics.ox.ac.uk>
2020-08-13 09:50:36 +0100

    Make some more touch ups

--- ESN.py
+++ ESN.py
@@ -7,7 +7,6 @@ import numpy as np
 import scipy.sparse as sparse
 import pandas as pd
 
-
 # global variables
 # This will change the initial condition used. Currently it starts from the first value
 shift_k = 0
@@ -33,9 +32,8 @@ res_params = {'radius': 0.1,
 
 # The ESN functions for training
 def generate_reservoir(size, radius, degree):
-    print("Generating reservoir")
     sparsity = degree/float(size)
-    A = sparse.rand(size, size, density=sparsity).todense()
+    A = sparse.rand(size, size, density=sparsity).toarray()
     vals = np.linalg.eigvals(A)
     e = np.max(np.abs(vals))
     A = (A/e) * radius
@@ -50,14 +48,20 @@ def reservoir_layer(A, Win, input, res_params):
 
 
 def train_reservoir(res_params, data):
+    print("train_reservoir: Generating reservoir")
     A = generate_reservoir(res_params['N'], res_params['radius'], res_params['degree'])
     q = int(res_params['N']/res_params['num_inputs'])
+
+    print("train_reservoir: Generating Win")
     Win = np.zeros((res_params['N'], res_params['num_inputs']))
     for i in range(res_params['num_inputs']):
         np.random.seed(seed=i)
         Win[i*q: (i+1)*q, i] = res_params['sigma'] * (-1 + 2 * np.random.rand(1, q)[0])
 
+    print("train_reservoir: Generating reservoir layer")
     states = reservoir_layer(A, Win, data, res_params)
+
+    print("train_reservoir: Training")
     Wout = train(res_params, states, data)
     x = states[:, -1]
     return x, Wout, A, Win
@@ -77,20 +81,29 @@ def train(res_params, states, data):
 
 
 def predict(A, Win, res_params, x, Wout):
+    # Define array for storing predictions at all time steps
     output = np.zeros((res_params['num_inputs'], res_params['predict_length']))
+
+    # Loop over all time steps
     for i in range(res_params['predict_length']):
+        if i % 100 == 0 or i == 0:
+            print(f"predict: Time step {i}")
+
         x_aug = x.copy()
+
+        # Perform additional nonlinearity
         for j in range(2, np.shape(x_aug)[0]-2):
             if (np.mod(j, 2) == 0):
-                x_aug[j] = (x[j-1]*x[j-2]).copy()
-        out = np.squeeze(np.asarray(np.dot(Wout, x_aug)))
-        output[:, i] = out
-        x1 = np.tanh(np.dot(A, x) + np.dot(Win, out))
-        x = np.squeeze(np.asarray(x1))
+                x_aug[j] = x[j-1]*x[j-2]
+
+        # Apply Wout to x_aug to form prediction
+        output[:, i] = np.dot(Wout, x_aug)
+
+        x = np.tanh(np.dot(A, x) + np.dot(Win, output[:, i]))
     return output, x
 
-# %%
 
+# %%
 
 print("Reading input data")
 dataf = pd.read_csv('3tier_lorenz_v3.csv', header=None)
@@ -100,6 +113,7 @@ data = np.transpose(np.array(dataf))
 
 # Train reservoir
 print("Training reservoir")
+print(f"res_params: {res_params}")
 x, Wout, A, Win = train_reservoir(res_params, data[:, shift_k:shift_k+res_params['train_length']])
 
 # %%
